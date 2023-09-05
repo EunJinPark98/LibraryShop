9aafbe77cbbc15e95884074c33ed087a54b84285
EJP <obliviscor29@naver.com>
2023-09-05 15:11:08 +0900

    모달창 닫으면 모달창에 데이터 지워지게함

---
+++ src/main/resources/static/js/main_header.js
@@ -0,0 +1,49 @@
+//주소 검색
+function openPost(){
+    new daum.Postcode({
+        oncomplete: function(data) {
+            document.querySelector('#memberAddr').value =  data.roadAddress;
+            
+        }
+    }).open();
+}
+
+//회원가입 시 데이터 유효성 검사
+function joinValidate(){
+    const joinForm = document.querySelector('#joinForm');       //form 안에 요소들은 name으로 접근가능!
+
+   
+    
+    if(joinForm.memberId.value == ''){
+        inputInvalidate('#id-error-div', '아이디를 입력해주세요.');
+        return;
+    }else if(joinForm.memberId.value.length < 4){
+        inputInvalidate('#id-error-div', '아이디는 4자 이상 입력해주세요.');
+        return;
+    }
+
+    // 정규식 테스트
+    var telRegex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+    const tel = joinForm.memberTels[0].value + '-' + joinForm.memberTels[1].value + '-' + joinForm.memberTels[2].value;
+
+    if(!(telRegex.test(tel))){
+        inputInvalidate('#tel-error-div', '연락처를 다시 입력해주세요.');
+        return;
+    }
+
+    //var emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+   joinForm.submit();
+}
+
+//validate 실패 시 메세지 설정
+function inputInvalidate(tagId, msg){
+    document.querySelector(tagId).style.display = 'block';
+    document.querySelector(tagId).textContent = msg;
+}
+
+//회원 가입 모달창 닫힐 때 마다 실행
+const joinModal = document.querySelector('#join-modal');
+joinModal.addEventListener('hidden.bs.modal', event => {
+    document.querySelector('#joinForm').reset(); 
+})
