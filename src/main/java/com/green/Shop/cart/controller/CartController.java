85f4053d40872d65a3fae32f5f256e00b74707b5
EJP <obliviscor29@naver.com>
2023-09-11 15:13:06 +0900

    장바구니 로그인여부체크

--- src/main/java/com/green/Shop/cart/controller/CartController.java
+++ src/main/java/com/green/Shop/cart/controller/CartController.java
@@ -1,4 +1,28 @@
 package com.green.Shop.cart.controller;
 
+import com.green.Shop.cart.service.CartService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/cart")
 public class CartController {
+    @Resource(name = "cartService")
+    private CartService cartService;
+
+    @ResponseBody
+    @GetMapping("putItem")
+    public void loginCheck(HttpSession session){
+        if(session.getAttribute("loginInfo")==null){
+
+        }else{
+
+        }
+    }
+
+
 }
--- src/main/java/com/green/Shop/cart/service/CartServiceImpl.java
+++ src/main/java/com/green/Shop/cart/service/CartServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("cartService")
-public class CartServiceImpl {
+public class CartServiceImpl implements CartService{
     @Autowired
     private SqlSessionTemplate sqlSession;
 }
